c0d8ae6e6ed8bec65f278cf92aaed8d2b60f4530
StuCodeGreen <m.kuznecovas@gmail.com>
2022-04-25 07:28:24 +0100

    added apollo client

---
+++ apollo-client.js
@@ -0,0 +1,16 @@
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+
+function createClient() {
+  return new ApolloClient({
+    link: new HttpLink({
+      uri: 'https://musical-pangolin-83.hasura.app/v1/graphql',
+      headers: {
+        'x-hasura-admin-secret': process.env.REACT_APP_HASURA_SECRET,
+      },
+    }),
+    cache: new InMemoryCache(),
+  });
+}
+
+export const client = createClient();
+
