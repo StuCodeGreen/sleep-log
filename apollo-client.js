aa3692733fbef5b2e87e5d3e6c709372e7296a1d
StuCodeGreen <m.kuznecovas@gmail.com>
2022-05-05 07:05:05 +0100

    testing apollo cache options

--- apollo-client.js
+++ apollo-client.js
@@ -2,32 +2,38 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 function createClient() {
   return new ApolloClient({
-    defaultOptions: {
-        query: {
-            fetchPolicy: 'network-only'
-        },
-        watchQuery: { fetchPolicy: "no-cache" },
-    },
+    // defaultOptions: {
+    //     query: {
+    //         fetchPolicy: 'network-only'
+    //     },
+    //     watchQuery: { fetchPolicy: "no-cache" },
+    // },
     link: new HttpLink({
       uri: 'https://musical-pangolin-83.hasura.app/v1/graphql',
       headers: {
         'x-hasura-admin-secret': process.env.HASURA_SECRET,
       },
     }),
-    cache: new InMemoryCache(),
     // cache: new InMemoryCache({
     //   typePolicies: {
-    //     Query: {
-    //       fields: {
-    //         logs: {
-    //           merge(existing = [], incoming) {
-    //             return { ...existing, ...incoming };
-    //           }
-    //         }
-    //       }
+    //     logs: {
+    //       keyFields: ["note"]
     //     }
     //   }
-    // })
+    // }),
+    cache: new InMemoryCache({
+      typePolicies: {
+        Query: {
+          fields: {
+            logs: {
+              merge(existing = [], incoming) {
+                return { ...existing, ...incoming };
+              }
+            }
+          }
+        }
+      }
+    })
   });
 }
 
